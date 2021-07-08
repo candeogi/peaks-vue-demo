a87194be966e56ff16f6c3bb016e088a7340b892
Giovanni Candeo <giovanni.candeo.1@studenti.unipd.it>
2021-07-08 17:35:24 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    id="overview-container"
+    style="width:100%;height:250px;"
+    ref="overview"
+  ></div>
+  <div
+    id="zoomview-container"
+    style="width:100%;height:250px;"
+    ref="zoomview"
+  ></div>
+  <audio
+    src="drmapan.wav"
+    style="width:100%"
+    controls
+  ></audio>
+  <div>
+    <button @click="zoomIn">Zoom in</button>
+    <button @click="zoomOut">Zoom out</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Peaks from "peaks.js";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      peakInstance: null,
+    };
+  },
+  mounted() {
+    this.createPeakInstance();
+  },
+  methods: {
+    createPeakInstance() {
+      const options = {
+        containers: {
+          overview: document.getElementById("overview-container"),
+          zoomview: document.getElementById("zoomview-container"),
+        },
+        mediaElement: document.querySelector("audio"),
+        dataUri: {
+          arraybuffer: "drmapan.dat", 
+        },
+        zoomLevels: [8, 16, 32, 64, 128, 256, 512],
+      };
+      const vm = this;
+      Peaks.init(options, function (err, peaks) {
+        console.log(err, peaks);
+        vm.peakInstance = peaks;
+      });
+    },
+    zoomIn() {
+      this.peakInstance.zoom.zoomIn();
+    },
+    zoomOut() {
+      this.peakInstance.zoom.zoomOut();
+    },
+  },
+};
 </script>
 
 <style>
